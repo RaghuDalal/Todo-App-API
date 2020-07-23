bb73724eed01caa36ef7cf5dd7a1f18413b3311c
Raghvender <raghvender002@gmail.com>
2020-07-23 13:09:31 +0530

    Added Create & List Todo's API

--- api/views.py
+++ api/views.py
@@ -33,3 +33,24 @@ def login(request):
             except:
                 token = Token.objects.create(user=user)
             return JsonResponse({'token':str(token)}, status=200)
+
+
+class TodoCompletedList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
